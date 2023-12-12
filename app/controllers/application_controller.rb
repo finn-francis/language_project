c42300218987b5f783859d28f4705d4f0f957dcb
finn-francis <finnfrancis123@gmail.com>
2023-12-12 16:13:51 +0000

    Created route path for logged in users
    How?
    - Created users controller#show action & view
    - Added view_object helper - which will be used to get variables into the views. This can later be used to send json to the front end.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, unless: :devise_controller?
+
+  helper_method :view_object
+
+  def view_object
+    @view_object ||= { current_user: current_user.as_json.symbolize_keys }
+  end
 end
