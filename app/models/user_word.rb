4fac1ccbdf6c37767c2cd6e82848708ae79d1da3
finn-francis <finnfrancis123@gmail.com>
2024-02-04 09:59:16 +0100

    Added status to user words

--- app/models/user_word.rb
+++ app/models/user_word.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class UserWord < ApplicationRecord
+  STATUS_VALUES = {
+    ignore: 0,
+    not_learned: 1,
+    recognised: 2,
+    learned: 3
+  }.freeze
+
   belongs_to :user
   belongs_to :word
+
+  enum status: STATUS_VALUES
 end
--- spec/models/user_word_spec.rb
+++ spec/models/user_word_spec.rb
@@ -5,4 +5,5 @@ require 'rails_helper'
 describe UserWord do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to belong_to(:word) }
+  it { is_expected.to define_enum_for(:status).with_values(UserWord::STATUS_VALUES) }
 end
